6c80689cd640daa3e5ec0736c5b30576a4ce863e
OShuaib <shuaib.olurode@decagon.dev>
2021-07-27 00:33:53 +0100

    my first go commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var name string = "Shuaib Olurode"
+var stack string = "Golang"
+var squad string = "eight"
+var year int = 2021
+
+func main()  {
+     fmt.Println("My name is", name,",I am a", stack,"developer, a member of squad", squad, "of year", year,".")
+}
+
+
+
